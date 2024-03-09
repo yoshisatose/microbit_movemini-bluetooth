07dbd5b3d1a665c5908f5e39911fb802d64b953f
Yoshihiro Sato <mail@yoshisato.se>
2024-03-09 13:41:14 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,21 +18,25 @@ bluetooth.onBluetoothDisconnected(function () {
     ServoLite.stop()
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
+    if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
+        basic.showString("F")
         ServoLite.forward()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_UP) {
         ServoLite.stop()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_DOWN) {
+        basic.showString("B")
         ServoLite.backward()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_2_UP) {
         ServoLite.stop()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_DOWN) {
+        basic.showString("V")
         ServoLite.left()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_3_UP) {
         ServoLite.stop()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_DOWN) {
+        basic.showString("H")
         ServoLite.right()
-    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
+    } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_4_UP) {
         ServoLite.stop()
     }
 })
